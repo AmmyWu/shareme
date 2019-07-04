d2d3ef806cc0cc77149dcedec43f1621563d8cf5
wumengwen <wumengwen@bytedance.com>
2019-07-05 00:07:59 +0800

    study

--- server/src/test/java/com/ammy/shareme/server/ServerApplicationTests.java
+++ server/src/test/java/com/ammy/shareme/server/ServerApplicationTests.java
@@ -3,6 +3,8 @@ package com.ammy.shareme.server;
 import com.ammy.shareme.server.util.ConstConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ServerApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(ServerApplicationTests.class);
 
     @Autowired
     ConstConfig config;
